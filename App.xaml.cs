9ff04ae52667a0d3006afc2eede24ef6648ffa65
frank <frank@CHEN-PC01>
2022-02-14 10:14:17 +0800

    添加项目文件。

---
+++ App.xaml.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using System.Media;
+using System.Runtime.InteropServices;
+using Microsoft.Win32;
+
+namespace GSFSRChanger
+{
+    /// <summary>
+    /// App.xaml 的交互逻辑
+    /// </summary>
+    public partial class App : Application
+    {
+       
+    }
+
+    /// <summary>
+    /// 原神的分辨率类
+    /// </summary>
+    public class GenShenResl
+    {
+        /// <summary>
+        /// 读取一个分辨率
+        /// </summary>
+        public static int[] ReadResl()
+        {
+            int[] Resl = new int[2];
+            Resl[0] = -1;
+            Resl[1] = -1;
+
+            try
+            {
+                RegistryKey GenshenKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\miHoYo\\原神");
+                foreach (string key
+                    in GenshenKey.GetValueNames())
+                {
+                    if (key.Contains("Screenmanager Resolution Height"))
+                        Resl[0] = (int)GenshenKey.GetValue(key);
+                    if (key.Contains("Screenmanager Resolution Width"))
+                        Resl[1] = (int)GenshenKey.GetValue(key);
+                }
+                GenshenKey.Close();
+                return Resl;
+            }
+            catch
+            {
+                SystemSounds.Beep.Play();
+                MessageBox.Show(
+                    "读出注册表错误",
+                    "失败",
+                    MessageBoxButton.OK);
+            }
+            return Resl;
+        }
+
+        /// <summary>
+        /// 写入一个分辨率
+        /// </summary>
+        public static bool SetResl(int Height,int Width)
+        {
+            if (SystemParameters.FullPrimaryScreenHeight < Height
+                || SystemParameters.FullPrimaryScreenWidth < Width)
+            {
+                SystemSounds.Asterisk.Play();
+                if (MessageBoxResult.No ==
+                    MessageBox.Show(
+                        "设置量超出屏幕范围，是否继续？",
+                        "警告",
+                        MessageBoxButton.YesNo))
+                    return false;
+            }
+            if ( Height<128|| Width<128)
+            {
+                SystemSounds.Asterisk.Play();
+                MessageBox.Show(
+                        "数值过小",
+                        "错误",
+                        MessageBoxButton.OK);
+            }
+
+            try
+            {
+                RegistryKey GenshenKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\\miHoYo\\原神",true);
+                foreach (string key
+                    in GenshenKey.GetValueNames())
+                {
+                    if (key.Contains("Screenmanager Resolution Height"))
+                        GenshenKey.SetValue(key, Height);
+                    if (key.Contains("Screenmanager Resolution Width"))
+                        GenshenKey.SetValue(key, Width);
+                }
+                GenshenKey.Close();
+                return true;
+            }
+            catch
+            {
+                SystemSounds.Beep.Play();
+                MessageBox.Show(
+                    "写入注册表错误",
+                    "失败",
+                    MessageBoxButton.OK);
+                return false;
+            }
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Media;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GSFSRChanger
+{
+    /// <summary>
+    /// MainWindow.xaml 的交互逻辑
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Conf_Click(object sender, RoutedEventArgs e)
+        {
+            if (!GenShenResl.SetResl(Convert.ToInt32(sHeight.Text)
+                , Convert.ToInt32(sWidth.Text)))
+                MessageBox.Show("写入失败", "错误", MessageBoxButton.OK);
+            else
+                SystemSounds.Asterisk.Play(); ;
+        }
+
+        private void Read_Click(object sender, RoutedEventArgs e)
+        {
+            sWidth.Text = "读取中...";
+            sHeight.Text = "读取中...";
+            int[] Resl = GenShenResl.ReadResl();
+            if (Resl[0] != -1 && Resl[1] != -1)
+            {
+                sHeight.Text = Resl[0].ToString();
+                sWidth.Text = Resl[1].ToString();
+                SystemSounds.Asterisk.Play();
+            }
+            else
+            {
+                sWidth.Text = "读取错误";
+                sHeight.Text = "读取错误";
+            }
+        }
+
+        private void PreSetting_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (PreSetting.SelectedIndex)
+            {
+                case 0:
+                    sWidth.Text = "3840";
+                    sHeight.Text = "2160";
+                    break;
+                case 1:
+                    sWidth.Text = "2560";
+                    sHeight.Text = "1440";
+                    break;
+                case 2:
+                    sWidth.Text = "1920";
+                    sHeight.Text = "1080";
+                    break;
+                case 3:
+                    sWidth.Text = "1280";
+                    sHeight.Text = "720";
+                    break;
+                case 4:
+                    sWidth.Text = "960";
+                    sHeight.Text = "640";
+                    break;
+                case 5:
+                    sWidth.Text = "720";
+                    sHeight.Text = "480";
+                    break;
+                default:
+                    sWidth.Text = "1920";
+                    sHeight.Text = "1080";
+                    break;
+            }
+        }
+    }
+}
